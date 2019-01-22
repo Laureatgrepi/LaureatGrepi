3a2c3dd9f58f5a7329dcd659d494afb598e7d676
Laureat Grepi <laureatgrepi@hotmail.com>
2019-01-22 22:17:17 +0200

    projekti laitettu

---
+++ Laureatgrepi/src/main/java/Laureatgrepi/Laureatgrepi/LaureatgrepiApplication.java
@@ -0,0 +1,14 @@
+package Laureatgrepi.Laureatgrepi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaureatgrepiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaureatgrepiApplication.class, args);
+	}
+
+}
+
---
+++ Laureatgrepi/src/main/java/Laureatgrepi/Laureatgrepi/WebController/WebController.java
@@ -0,0 +1,22 @@
+package Laureatgrepi.Laureatgrepi.WebController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+
+	@GetMapping("/index")
+	public String indexPage() {
+		return "index";
+	}
+	@GetMapping("/contact")
+	public String contactPage() {
+		return "contact";
+	}
+	@GetMapping("/CV")
+	public String cv() {
+		return "CV";
+	}
+}
