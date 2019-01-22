3af2f1b4b0998fea7958672f3ee86e8e7e5215b4
Laureat Grepi <laureatgrepi@hotmail.com>
2019-01-22 22:22:20 +0200

    testi

--- Laureatgrepi/src/main/java/Laureatgrepi/Laureatgrepi/application/LaureatgrepiApplication.java
+++ Laureatgrepi/src/main/java/Laureatgrepi/Laureatgrepi/application/LaureatgrepiApplication.java
@@ -1,4 +1,4 @@
-package Laureatgrepi.Laureatgrepi;
+package Laureatgrepi.Laureatgrepi.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
